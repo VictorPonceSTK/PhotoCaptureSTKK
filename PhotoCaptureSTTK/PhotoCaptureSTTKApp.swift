5821287d9e14eee630a0aec89b62e083bb1f1249
Victor <victor.ponce@softtek.com>
2024-02-21 11:19:53 -0600

    Initial Commit

---
+++ PhotoCaptureSTTK/PhotoCaptureSTTKApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCaptureSTTKApp.swift
+//  PhotoCaptureSTTK
+//
+//  Created by Victor David Ponce Quintanilla on 21/02/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotoCaptureSTTKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
